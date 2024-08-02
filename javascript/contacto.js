437fff10c0af0b0f022c52c7484a86c106a05997
Alexis Urbina <alexisurbinaa435@gmail.com>
2024-08-02 04:22:36 -0300

    Cambios realizados

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -16,26 +16,26 @@ btn.addEventListener("click", (e) => {
 function validar(e) {
     let invalidacion = /^[a-zA-ZéÉáÁíóúÍÓÚÑñ\s]*$/;
     let invalidacion2 = /^([0-9])*$/;
-    let email = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{3,63}$/i;
+    let email = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{3,4}$/i;
     if (nombreYapellido.value == "" || correo.value == "" || telefono.value == "" || consulta.value == "") {
         e.preventDefault();
         alert("Por favor, completar todas las casillas");
     } else if (nombreYapellido.value.length < 5) {
         e.preventDefault();
-        alert("Ingrese un nombre con 6 caracteres o más ");
+        alert("Ingrese un nombre con 5 caracteres o más ");
     } else if (!invalidacion.test(nombreYapellido.value)) {
         e.preventDefault();
         alert("Nombre no valido");
     } else if (!email.test(correo.value)) {
         e.preventDefault();
         alert("Correo no valido ");
-    } else if (telefono.value.length < 9) {
+    } else if (telefono.value.length < 10) {
         e.preventDefault();
         alert("Ingrese telefono con 10 o más numeros");
     } else if (!invalidacion2.test(telefono.value)) {
         e.preventDefault();
         alert("Numero de telefono no valido");
-    } else if (consulta.value.length < 9) {
+    } else if (consulta.value.length < 10) {
         e.preventDefault();
         alert("Ingrese 10 caracteres o mas para su consulta");
     } else {
