095eba3f060d99c07060a787f4853d12049bd245
Alexis Urbina <alexisurbinaa435@gmail.com>
2024-08-02 15:54:40 -0300

    Codigo js contacto mejorado

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -3,19 +3,18 @@ let nombreYapellido = document.getElementById("nombre");
 let correo = document.getElementById("email");
 let telefono = document.getElementById("telefono");
 let consulta = document.getElementById("consulta");
-let btn = document.getElementById("btn");
+let Formulario = document.getElementById("formulario");
+
 let informacion = [];
 
-btn.addEventListener("click", (e) => {
+Formulario.addEventListener("submit", (e) => {
     informacion[0] = `Su nombre y apellido es: ${nombreYapellido.value} `;
     informacion[1] = `El correo electronico es: ${correo.value} `;
     informacion[2] = `El numero de telefono es: ${telefono.value} `;
     informacion[3] = `Su consulta es: ${consulta.value}`;
-    validar(e);
-})
-function validar(e) {
-    let invalidacion = /^[a-zA-ZéÉáÁíóúÍÓÚÑñ\s]*$/;
-    let invalidacion2 = /^([0-9])*$/;
+
+    let validacion = /^[a-zA-ZéÉáÁíóúÍÓÚÑñ\s]*$/;
+    let validacion2 = /^([0-9])*$/;
     let email = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{3,4}$/i;
     if (nombreYapellido.value == "" || correo.value == "" || telefono.value == "" || consulta.value == "") {
         e.preventDefault();
@@ -23,7 +22,7 @@ function validar(e) {
     } else if (nombreYapellido.value.length < 5) {
         e.preventDefault();
         alert("Ingrese un nombre con 5 caracteres o más ");
-    } else if (!invalidacion.test(nombreYapellido.value)) {
+    } else if (!validacion.test(nombreYapellido.value)) {
         e.preventDefault();
         alert("Nombre no valido");
     } else if (!email.test(correo.value)) {
@@ -32,7 +31,7 @@ function validar(e) {
     } else if (telefono.value.length < 10) {
         e.preventDefault();
         alert("Ingrese telefono con 10 o más numeros");
-    } else if (!invalidacion2.test(telefono.value)) {
+    } else if (!validacion2.test(telefono.value)) {
         e.preventDefault();
         alert("Numero de telefono no valido");
     } else if (consulta.value.length < 10) {
@@ -43,4 +42,6 @@ function validar(e) {
         let blob = new Blob([informacion], { type: "text/plain;charset=utf-8" });
         saveAs(blob, "contacto.txt");
     }
-}
+})
+
+
